26cb543b1af7e277bd3c82585516d2b5b2da43e3
DFaltGP <skyzzlockzz@gmail.com>
2024-02-07 20:19:01 -0300

    websockets and redis integrated on the votes system

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,18 +1,27 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
+import websocket from '@fastify/websocket';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
-import cookie from '@fastify/cookie';
+import { pollResults } from './ws/poll-result';
 
+// Calling Fastify
 const app = fastify();
 
+// Functionalities
 app.register(cookie, {
     secret: "polls-secret-nlw",
     hook: "onRequest",
 })
 
+app.register(websocket)
+
+// Routes
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
+// starting the server on port: XXXX
 app.listen({port: 3333}).then(() => console.log('server running on port 3333'))
